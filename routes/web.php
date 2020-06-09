2a6433b891348d71df710dbfd35d3daa18bf826f
Weverton <w.m.pires@hotmail.com>
2020-06-09 18:47:11 -0300

    Aula 06 - Criando Tabelas One To Many

--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,14 @@ Route::get('one-to-one', 'OneToOneController@oneToOne')->name('one-to-one');
 Route::get('one-to-one-inverse', 'OneToOneController@oneToOneInverse')->name('one-to-one-inverse');
 Route::get('one-to-one-insert', 'OneToOneController@oneToOneInsert')->name('one-to-one-insert');
 
+/*
+|
+| One to Nany
+|
+*/
+
+Route::get('one-to-many', 'OneToManyController@oneToMany')->name('one-to-one');
+
 Route::get('/', function () {
     return view('welcome');
 });
